c460c51fdae8682043aa8d81d780091400e0977a
Dev-Justin-Adams <devadamsjustin@gmail.com>
2024-09-15 19:13:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+/*console.log('Hello World!');
+console.log('Hello World!');
+const toggleButton = document.getElementById('mode-toggle');
+const currentMode = localStorage.getItem('mode');
+
+if (currentMode === 'dark') {
+    document.body.classList.add('dark-mode');
+    toggleButton.textContent = '🌙';
+}
+
+toggleButton.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('mode', 'dark');
+        toggleButton.textContent = '🌙';
+    } else {
+        localStorage.setItem('mode', 'light');
+        toggleButton.textContent = '🌞';
+    }
+});*/
